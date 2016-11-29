31d8cda72641f22d78f2bb5a197b11a91cf29fbb
rektide <rektide@voodoowarez.com>
2016-11-28 22:29:28 -0500

    p-filter: thunkify.

--- p-thunk.js
+++ p-thunk.js
@@ -12,6 +12,8 @@ getter({
 	"cancelable": "./p-cancelable",
 	"p-each-series": "./p-each-series",
 	"eachSeries": "./p-each-series",
+	"p-filter": "./p-filter",
+	"filter": "./p-filter",
 	"p-immediate": "./p-immediate",
 	"immediate": "./p-immediate",
 	"p-is-promise": "./p-is-promise",
